78d74156d0857292adc938ca442bcb699e354331
anonimys2020 <vladislavargun2007@gmail.com>
2023-04-25 12:59:53 +0300

    edit0

--- script.sh
+++ script.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-
 if [ -z "$1" ]; then
   echo "Usage: $0 <path_to_file.sh>"
   exit 1
 fi
-
-# Шифрование содержимого файла в кодировку Hexadecimal c использованием 
-опции -c
+# Шифрование содержимого файла в кодировку Hexadecimal c использованием опции -c
 code_hex=$(xxd -p -c 10000 "$1" | sed 's/\(..\)/\\x\1/g')
-
 # Создание нового .sh файла и запись зашифрованной строки в переменную 
 code_hex
 new_file="$1.enc.sh"
@@ -19,6 +15,4 @@ code_bytes=\$(echo \$code_hex | sed 's/\\\\x//g' | xxd -r -p)
 code_str=\$(echo \$code_bytes | tr -d '\n')
 eval \$code_str
 EOF
-
 echo "Encrypted script saved to $new_file"
-
