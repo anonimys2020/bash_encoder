56acc299db8cdd99456dc479d046cd165a7294d1
anonimys2020 <vladislavargun2007@gmail.com>
2023-05-01 16:42:34 +0300

    Fixed Bugs

--- script.sh
+++ script.sh
@@ -3,13 +3,16 @@ if [ -z "$1" ]; then
   echo "Usage: $0 <path_to_file.sh>"
   exit 1
 fi
-# Шифрование содержимого файла в кодировку Hexadecimal c использованием опции -c
-code_hex=$(xxd -p -c 10000 "$1" gi| sed 's/\(..\)/\\x\1/g')
+# Шифрование скрипта
+code_hex=$(sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/;/g' -e 's/\(do\|then\|else\|fi\|done\)\( *\)$/\1\n/g' -e 's/;;/;/g' -e 's/;/; /g' -e 's/done;/done/g' -e 's/do;/do/g' < "$1" | xxd -p)
 # Создание нового .sh файла и запись зашифрованной строки в переменную code_hex
 new_file="$1.enc.sh"
-echo "#!/bin/bash
-code_hex=\"$code_hex\"
-code_bytes=\$(echo \$code_hex | sed 's/\\\\x//g' | xxd -r -p)
+cat <<EOF >"$new_file"
+#!/bin/bash
+code_hex="$code_hex"
+code_bytes=\$(echo \$code_hex | sed 's/..../& /g' | tr -d ' ' | xxd -r -p)
 code_str=\$(echo \$code_bytes | tr -d '\n')
-eval \$code_str" > $new_file
+eval \$code_str
+EOF
+chmod +x "$new_file"
 echo "Encrypted script saved to $new_file"
