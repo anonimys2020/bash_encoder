8c5e814b4f255ac15ec6b1604f5b4c81f81cd441
anonimys2020 <vladislavargun2007@gmail.com>
2023-05-07 17:22:36 +0300

    Final version

--- script.sh
+++ script.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-if [ -z "$1" ]; then
+if [ -z "$@" ]; then
   echo "Usage: $0 <path_to_file.sh>"
   exit 1
 fi
-# Шифрование скрипта
-code_hex=$(sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/;/g' -e 's/\(do\|then\|else\|fi\|done\|function\|select\|time\|until\|while\|in\)\( *\)[;]*$/\1\n/g' -e 's/;;/;/g' -e 's/;/; /g' -e 's/do;/do/g' -e 's/then;/then/g' -e 's/else;/else/g' -e 's/function;/function/g' -e 's/select;/select/g' -e 's/time;/time/g' -e 's/until;/until/g' -e 's/while;/while/g' < "$1" | xxd -p)
 # Создание нового .sh файла и запись зашифрованной строки в переменную code_hex
+code_hex=$(sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/;/g' -e 's/\(do\|then\|else\|fi\|done\|select\|time\|until\|while\|in\)\( *\)[;]*$/\1\n/g' -e 's/;;/;/g' -e 's/;/; /g' -e 's/do;/do/g' -e 's/then;/then/g' -e 's/else;/else/g' -e 's/\bfunction\b[[:space:]]*([^)]*)[[:space:]]*{[[:space:]]*/&\n/g' -e 's/time;/time/g' -e 's/until;/until/g' -e 's/while;/while/g' -e 's/\(\bif\b\|\belif\b\)\([^ ]\)/\1 \2/g' -e 's/\(\bif\b\)[ ]\{1,\}\(\[\)/\1 \2/g' -e 's/\[\([^ ]\)/\[ \1/g' -e 's/\b=\b/=/g' -e 's/\bfor\b\(.*;\)\([^ ]\)/for \1 \2/g' -e 's/\bwhile\b\([^ ]\)/while \1/g' -e 's/:/; /g' -e 's/break;/break; /g' -e 's/\<-lt\>/</g' -e 's/\([^ ]\);/\1; /g' -e 's/\(echo ".*"\);/\1 ;/g' -e 's/{;/{/g' < "$1" | xxd -p)
 new_file="$1.enc.sh"
 cat <<EOF >"$new_file"
 #!/bin/bash
